f67a21fe7bf23499fa40ec24e0ac2edc3330c51c
Ebubekir Karataş <karatasebu47@gmail.com>
2021-08-01 18:23:33 +0300

    slider initial scroll added

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,19 +1,28 @@
 export default function slider(list) {
   const prevBtnEl = document.querySelector("#prev");
   const nextBtnEl = document.querySelector("#next");
-  let position = null;
-  list.clientWidth > 960 ? (position = 960) : (position = 480);
+  let scrollAmount = null;
+
+  function init() {
+    prevBtnEl.disabled = true;
+    if (nextBtnEl.disabled) nextBtnEl.disabled = false;
+    list.classList.remove("-scroll");
+    list.scrollLeft = 0;
+    list.clientWidth > 960 ? (scrollAmount = 960) : (scrollAmount = 480);
+  }
+  init();
 
   nextBtnEl.addEventListener("click", function () {
+    list.classList.add("-scroll");
     prevBtnEl.disabled = false;
-    list.scrollLeft += position;
+    list.scrollLeft += scrollAmount;
     if (list.scrollLeft + list.clientWidth * 1.7 > list.scrollWidth) {
       this.disabled = true;
     }
   });
   prevBtnEl.addEventListener("click", function () {
     nextBtnEl.disabled = false;
-    list.scrollLeft -= position;
+    list.scrollLeft -= scrollAmount;
     if (list.scrollLeft < list.clientWidth) {
       this.disabled = true;
     }
