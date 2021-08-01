67a51860442c9f7b58dd8bfc0d3204b8c88d9634
Ebubekir Karataş <karatasebu47@gmail.com>
2021-08-01 20:24:00 +0300

    nonfree shipping div removed

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,23 +1,24 @@
 import free from "../assets/svg/free.svg";
-import nonfree from "../assets/svg/non-free.svg";
 
 export default function renderCard(product) {
   const popupEl = document.querySelector("#popup");
   const card = `
   <div class="card">
-  <div class=card__section>
+  <div class="card__section">
   <img class="card__img" loading="lazy" src="${product.image}" alt="${
     product.name
   }"/>
   <p class="card__title">${product.name}</p>
   </div>
-  <div class=card__section>
+  <div class="card__section -bottom">
   <span class="card__price">${product.priceText}</span>
-  <div class="card__shipping">${
+  ${
     product.params.shippingFee === "FREE"
-      ? `<img src="${free}" alt="shipping"></img><p>Ücretsiz Kargo</p>`
-      : `<img src="${nonfree}" alt="shipping"></img><p>Ücretli Kargo</p>`
-  }</div>
+      ? `<div class="card__shipping">
+      <img src="${free}" alt="shipping"></img><p>Ücretsiz Kargo</p>
+      </div>`
+      : ""
+  }
   <button class="card__btn" onclick="addItem()">Sepete Ekle</button>
   </div>
   </div>
